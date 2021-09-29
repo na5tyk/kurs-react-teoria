e377366c1f0497ef3d825dbd997224b0920ca919
Patryk <patrykluba97@gmail.com>
2021-09-29 23:13:56 +0200

    learn JSX v2 :

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const dogName = 'Burek';
+  const paragraf = <p>Pies wabi się {dogName}</p>;
+
+  const isDog = true;
+
+  const element = <h1 className="greeting">Hello world</h1>;
+  const element2 = React.createElement('h1', { className: 'greeting'}, 'Hello world')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {element}
+      {element2}
+      {paragraf}
+      {2+2}
+      {dogName.toUpperCase()}
+      {isDog ? <p>Tak, to jest</p> : <p>Nie, nie jes to pies</p>}
     </div>
   );
 }
