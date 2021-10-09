957bfda53762a34d32e95ea1f597e61f552a4153
Patryk <patrykluba97@gmail.com>
2021-10-09 21:26:42 +0200

    2.4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Article from './components/Article';
 import ArticleBox from './components/ArticleBox';
 
 function App() {
   return (
     <div>
+      <Article title="Tytuł komponetu funkcyjnego" description="Opis funkcyjny 1" />
       <ArticleBox title="Tytuł 1" description="Opis 1" />
       <ArticleBox title="Tytuł 2" description="Opis 2" />
       <ArticleBox title="Tytuł 3" description="Opis 3" />
