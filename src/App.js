d80396f8c9208e816b805ffefd62474f82635792
Patryk <patrykluba97@gmail.com>
2021-10-26 20:39:00 +0200

    react lazy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import HandleEvent from './components/HandleEvent';
+import React, { Suspense } from 'react';
+
+const HandleEvent = React.lazy(() => import('./components/HandleEvent'))
 
 function App() {
   return (
     <div>
       Obsługa zdarzeń
-      <HandleEvent />
+      <Suspense fallback={<div>Ładowanie</div>}>
+        <HandleEvent />
+      </Suspense>
     </div>
   );
 }
